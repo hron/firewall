467f265bf96c5034907e092f6d2b0f3268534647
Aleksei Gusev <aleksei.gusev@gmail.com>
2015-06-14 18:31:37 +0300

    Add route rule for openvpn

--- recipes/openvpn.rb
+++ recipes/openvpn.rb
@@ -5,7 +5,9 @@ firewall_rule 'allow world to openvpn' do
   action [:allow]
 end
 
-firewall_rule 'allow vpn clients to access world' do
-  direction in: 'tun0', out: 'eth0'
-  action [:allow]
-end
+# ufw route allow in on tun0 out on eth0
+# firewall_rule 'allow vpn clients to access world' do
+#   direction in: 'tun0', out: 'eth0'
+#   action [:allow]
+# end
+shell_out!('ufw route allow in on tun0 out on eth0')
